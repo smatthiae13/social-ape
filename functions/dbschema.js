8365755b5752af95c53d55dc933bac14cf10548c
Sam Matthiae <rocketsam13@yahoo.com>
2019-06-30 18:44:37 -0500

    post

---
+++ functions/dbschema.js
@@ -0,0 +1,12 @@
+let db = {                              //this is just for reference on how we want the db to look like
+    scream: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2019-03-15T11:46:01.018Z',          //ISOString
+            likeCount: 5,
+            commentCount: 2
+        
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -3,43 +3,58 @@ const admin = require('firebase-admin');       //needs access to the database
 
 admin.initializeApp(); //we have access to admin object                 //to use admin we need to initialize the application
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {  //use postman to check with GET
- response.send("Hello Sam!");
-});
 
-//fetch therse
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('screams')       //getting info from our collection, in example from firebase it is labed as db.collection('name of collection')
+const express = require('express');
+const app = express();
+
+
+//fetch these
+app.get('/screams', (req, res) => {         //first paramenter is the name of the route and the second is the handler
+    admin
+    .firestore()
+    .collection('screams')      //getting info from our collection, in example from firebase it is labed as db.collection('name of collection')
+    .orderBy('createdAt', 'desc') 
     .get()
     .then((data) => {
         let screams = [];
         data.forEach((doc) => {
-            screams.push(doc.data());    // (doc is jusst a reference)data is a function that returns the data instde the document
-        });         //after for loop, the screams array should be filled with the data of the screams
-        return res.json(screams);            //now we have to return them
+            screams.push({          //try to use spread operator --...doc.data() supported after node 6
+                screamId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
+            });                            
+        });                              //after for loop, the screams array should be filled with the data of the screams
+        return res.json(screams);        //now we have to return them
     })
     .catch(err => console.error(err));
-});
+})         
 
-exports.createScream = functions.https.onRequest((req, res) => {
+
+
+app.post('/scream', (req, res) => {
+   
     const newScream = {                                     //this will be our object
         body: req.body.body,
-        userhandle: req.body.userhandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
     };
 
-    admin.firestore()    //persist it from our db
+    admin
+    .firestore()    //persist it from our db
     .collection('screams')
     .add(newScream)      //takes a json obj and add it to the database  - returns a promise
     .then((doc) => {        //givers us a document reference as a response
         res.json({ message: `document ${doc.id} created successfully`});                    //if we are here, the docusmtn has been created, and we can return a response
     })             
     .catch(err => {
-        res.status(500).json({ error: `something went wrong`});  //because of an error, we need to return a status code
+        res.status(500).json({ error: 'something went wrong'});  //because of an error, we need to return a status code
         console.err(err);
     });
 
 });
+
+
+//
+exports.api = functions.https.onRequest(app);       // this will turn into multiple routes-a container for the app
+
